e25cc255d3ebb3207919f792adeaa1925ca9a2a1
Fernando Rubilar Z <frubilar@icci.cl>
2017-01-05 12:13:44 -0300

    CHG: main generador de ficheros para que el show muestre segun procesador CHG: node el caracter que elimina sera el '\n' y no el ultimo
    Signed-off-by: Fernando Rubilar Z <frubilar@icci.cl>

--- main.rb
+++ main.rb
@@ -2,14 +2,14 @@ require 'geometry'
 require_relative './triangulos'
 require_relative './node'
 require 'mpi'
-node = ARGV[0] # recive el nombre del fichero .node
-mesh = ARGV[1] # recive el nombre del fichero .mesh
-grado = ARGV[2]
-MPI.Init
-world = MPI::Comm::WORLD
-#node = 'vertices'
-#mesh= 'triangulos'
-#grado = 12
+#node = ARGV[0] # recive el nombre del fichero .node
+#mesh = ARGV[1] # recive el nombre del fichero .mesh
+#grado = ARGV[2]
+#MPI.Init
+#world = MPI::Comm::WORLD
+node = 'vertices'
+mesh= 'triangulos'
+grado = 12
 preproceso = Node.new(node,mesh) # instancia un objeto con los datos node y mesh 
 node = preproceso.node # prepocesamiento node
 mesh = preproceso.mesh # prepoceso mesh
@@ -54,13 +54,14 @@ end # complejidad O(2n)
 
 def dividirTriangulosPorNodo(tamano,cantidad)
   ma=[]
-  n=[]
+  resto = -1*(cantidad%tamano)
   i = 1
-  procesador = 0
-  
+  procesador = 1
   loop do
     tamano.times do
-      ma << i.to_i<<procesador.to_i
+      n=[]
+      n << i.to_i<<procesador.to_i
+      ma << n
       i+=1
     end
     procesador +=1    
@@ -70,25 +71,54 @@ def dividirTriangulosPorNodo(tamano,cantidad)
     ma.delete_at(ma.length-1)
     break if cantidad >= ma.length
   end
+  puts procesador
+  for i in resto..1
+    if i == 0
+      break
+    end
+    ma[i][1]=procesador-2
+  end
   return ma
+end
+
+def generarPart(mesh)
+  File.open('archivo.part','w') do |f|
+    f.puts mesh.length.to_s+' '+mesh[-1][1].to_s
+    for i in 0..mesh.length-1
+      f.puts mesh[i][0].to_s+' '+mesh[i][1].to_s
+    end
+  end
+end
+
+def generarEle(mesh)
+  File.open('archivo.ele','w') do |f|
+    f.puts (mesh.length-1).to_s+' 3'
+    for i in 1..mesh.length-1
+      f.puts i.to_s+' '+mesh[i][0].to_s+' '+mesh[i][1].to_s+' '+mesh[i][2].to_s
+    end
+  end
+end
+
+def generarNode(node)
+  File.open('archivo.node','w') do |f|
+    f.puts (node.length-1).to_s+' 2'
+    for i in 1..node.length-1
+      f.puts i.to_s+' '+node[i][1].to_s+' '+node[i][2].to_s
+    end
+  end
 end
 
 uno = toEdge(node[1],node[2])
 dos = toEdge(node[1],node[3])
 tres = toEdge(node[2],node[3])
 t = Triangulos.new(uno.length, dos.length, tres.length, grado.to_i) 
-cantidad = mesh[0][0].to_i/world.size
+cantidad = mesh[0][0].to_i/3
 mesh_nodo = dividirTriangulosPorNodo(cantidad.to_i,mesh[0][0].to_i)
+generarPart(mesh_nodo)
+generarNode(node)
+generarEle(mesh)
 
-j=0
-for i in mesh_nodo.length-1
-  if j==mesh_nodo[1]
-    puts mesh_nodo[]
-  end
-end
-puts mesh_nodo.last
-
-MPI.Finalize
+#MPI.Finalize
 
 
 
