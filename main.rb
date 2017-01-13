2d866211b833058b8253ef867000e4153a3deb09
Fernando Rubilar Z <frubilar@icci.cl>
2017-01-13 12:51:40 -0300

    cambios minimos
    Signed-off-by: Fernando Rubilar Z <frubilar@icci.cl>

--- main.rb
+++ main.rb
@@ -2,6 +2,9 @@ require 'geometry'
 require_relative './triangulos'
 require_relative './node'
 require 'mpi'
+if defined?(NumRu::NArray)
+  include NumRu
+end
 #node = ARGV[0] # recive el nombre del fichero .node
 #mesh = ARGV[1] # recive el nombre del fichero .mesh
 grado = ARGV[0]
@@ -56,7 +59,7 @@ def dividirTriangulosPorNodo(tamano,cantidad)
   ma=[]
   resto = -1*(cantidad%tamano)
   i = 1
-  procesador = 1
+  procesador = 0
   loop do
     tamano.times do
       n=[]
@@ -80,16 +83,16 @@ def dividirTriangulosPorNodo(tamano,cantidad)
     if i == 0
       break
     end
-    ma[i][1]= (cantidad/tamano).to_i
+    ma[i][1]= ((cantidad/tamano).to_i)-1
   end
   return ma
 end
 
 def generarPart(mesh)
   File.open('archivo.part','w') do |f|
-    f.puts mesh.length.to_s+' '+mesh[-1][1].to_s
+    f.puts mesh.length.to_s+' '+(mesh[-1][1]+1).to_s
     for i in 0..mesh.length-1
-      f.puts mesh[i][0].to_s+' '+mesh[i][1].to_s
+      f.puts mesh[i][0].to_s+' '+(mesh[i][1]+1).to_s
     end
   end
 end
@@ -143,18 +146,72 @@ def candidatos_a_refinar(mesh,node,grado)
   end
   return lista
 end
+def triangulosArefinar(refinar) #indice del triangulo en el vector
+  lista=[]
+  for i in 0..refinar.length-1
+    if refinar[i]==1
+      lista<<i+1
+    end
+  end
+  return lista
+end #complejidad O(n)
 
 uno = toEdge(node[1],node[2])
 dos = toEdge(node[1],node[3])
 tres = toEdge(node[2],node[3])
 t = Triangulos.new(uno.length, dos.length, tres.length, grado.to_i) 
 cantidad = mesh[0][0].to_i/world.size
+resto = mesh[0][0].to_i%world.size
 mesh_nodo = dividirTriangulosPorNodo(cantidad.to_i,mesh[0][0].to_i)
+candidato = candidatos_a_refinar(mesh,node,grado)
+triangulos_a_ref = triangulosArefinar(candidato)
 generarPart(mesh_nodo)
 generarNode(node)
 generarEle(mesh)
+def pertenezco_al_nodo(tr_a_refinar,mesh_nodo,rank)
+  for i in 0..mesh_nodo.size-1
+    if tr_a_refinar == mesh_nodo[i][0]
+      if rank== mesh_nodo[i][1]
+        return 1
+      end
+    end
+  end
+  return 0
+end
 
-puts (candidatos_a_refinar(mesh,node,grado)).reduce(:+)
+rank = world.rank
+
+for i in 0..rank
+  if i == rank
+    sum = 0
+    a = NArray.int(cantidad.to_i+resto)
+    k=0
+    for j in 0..mesh_nodo.size-1
+      if mesh_nodo[j][1]==rank
+        a[k]=mesh_nodo[j][0]
+        k+=1
+      end
+    end
+    ref = 0
+    for j in 0..(triangulos_a_ref.length)-1
+      ref = ref+pertenezco_al_nodo(triangulos_a_ref[j],mesh_nodo,rank)
+    end
+    puts ref
+    world.Send(a, 0, 1)
+  end
+end
+if rank == 0
+  (world.size).times do |i|
+    a = NArray.int(cantidad.to_i+resto)
+    world.Recv(a, i, 1)
+    
+    p a
+  end
+end
+
+
+
+
 MPI.Finalize
 
 
